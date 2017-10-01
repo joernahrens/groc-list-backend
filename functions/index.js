9a4820227c80b891506cc74ee31975b19e07c79e
Jörn Ahrens <joern.ahrens@jimdo.com>
2017-10-01 21:18:31 +0200

    added first function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,16 @@
-"use strict";
+'use strict';
+Object.defineProperty(exports, "__esModule", { value: true });
 var functions = require('firebase-functions');
+var fbAdmin = require('firebase-admin');
+fbAdmin.initializeApp(functions.config().firebase);
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+exports.onNewInvite = functions.database.ref('/lists/{authId}/{listId}/invites/{inviteId}')
+    .onWrite(function (snapshot) {
+    var email = snapshot.val().email;
+    console.log('new invite: ' + email);
+});
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,10 @@
+'use strict';
+
+import {DeltaSnapshot} from 'firebase-functions/lib/providers/database';
+
 const functions = require('firebase-functions');
+const fbAdmin = require('firebase-admin');
+fbAdmin.initializeApp(functions.config().firebase);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +12,9 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.onNewInvite = functions.database.ref('/lists/{authId}/{listId}/invites/{inviteId}')
+    .onWrite((snapshot: DeltaSnapshot) => {
+        const email: string = snapshot.val().email;
+        console.log('new invite: ' + email);
+    });
