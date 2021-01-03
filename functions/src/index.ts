8ed6da0823594e62d78c48b4a2564a19ff0b0977
Jörn Ahrens <joern.ahrens@gmail.com>
2021-01-03 16:05:29 +0100

    Add push notification for invites

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,54 +4,17 @@ import DocumentData = admin.firestore.DocumentData;
 
 admin.initializeApp(functions.config().firebase);
 
-exports.onNewInvite = functions.database.ref('/lists/{authId}/{listId}/invites/{inviteId}')
-    .onWrite((change: functions.Change<functions.database.DataSnapshot>, context: functions.EventContext) => {
-        // ignore deletion
-        if (change.after.exists()) {
-            const email: string = change.after.val().email;
-            const listId: string = context.params.listId;
-            const authId: string = context.params.authId;
-
-            return Promise.all(
-                [admin.auth().getUser(authId),
-                    change.after.ref.parent!!.parent!!.child('name').once('value')])
-                .then((args: Array<any>) => {
-                    admin.database().ref('/shared/' + email.replace(/\./g, ','))
-                        .push({list: listId, owner: authId, ownerMail: args[0].email, name: args[1].val()});
-                })
-                .then(() => {
-                    change.after.ref.parent!!.parent!!.child('fellows').once('value')
-                        .then((data: admin.database.DataSnapshot) => {
-                            if (data.exists()) {
-                                return data.ref.set(data.val() + ',' + email);
-                            } else {
-                                return data.ref.set(email);
-                            }
-                        })
-                        .catch((error: any) => {
-                            console.error('something went wrong:');
-                            console.error(error);
-                        });
-                })
-                .then(() => change.after.ref.remove())
-                .catch((error: any) => {
-                    console.error('something went wrong:');
-                    console.error(error);
-                });
-        } else {
-            return Promise.resolve();
-        }
-    });
-
 exports.onNewInviteFirestore = functions.firestore.document('users/{email}/lists/{list}/shares/{share}')
     .onCreate((change, context) => {
         const mailAddress = change.id;
         console.log("Mail address: " + mailAddress);
+        let listKey = "";
 
         return Promise.all([change.ref.parent.parent.get(),
             change.ref.parent.parent.parent.parent.get()]
         )
             .then((args: Array<admin.firestore.DocumentSnapshot<DocumentData>>) => {
+                listKey = args[0].id
                 return admin.firestore().doc(`users/${mailAddress}/sharedLists/${args[1].id}🤓${args[0].id}`)
                     .set({
                         listOwner: args[1].id,
@@ -59,7 +22,25 @@ exports.onNewInviteFirestore = functions.firestore.document('users/{email}/lists
                     })
             })
             .then(() => {
-                console.log('All fine 👍');
+                console.log('All fine 👍, trying to send a push notification');
+                return admin.firestore().collection(`users/${mailAddress}/pushToken`).get()
+            })
+            .then((tokens) => {
+                const payload = {
+                    notification: {
+                        title: 'New invite!',
+                        body: `You have been invited to the list ${listKey}!`
+                    }
+                };
+                if (!tokens.empty) {
+                    const pushTokens = [];
+                    tokens.docs.forEach((value => {
+                        pushTokens.push(value.id)
+                    }))
+                    return admin.messaging().sendToDevice(pushTokens, payload) as Promise<any>
+                } else {
+                    return Promise.resolve()
+                }
             })
             .catch((error: any) => {
                 console.error('something went wrong:');
